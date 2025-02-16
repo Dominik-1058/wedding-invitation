6707259dd4bc8752f020148de41f7db0b7cca4b2
unknown <reitererdominik@gmail.com>
2025-02-16 14:46:06 +0100

    removed unused code

--- src/pages/PicturePage.tsx
+++ src/pages/PicturePage.tsx
@@ -5,14 +5,6 @@ import styles from '../css/App.module.css'
 import { FileDrop } from '../components/FileDrop';
 
 const PicturePage = () => {
-    const [message, setMessage] = useState("");
-
-    useEffect(() => {
-      fetch("/api/")  // Proxy will forward to FastAPI backend
-          .then((res) => res.json())
-          .then((data) => setMessage(data.message));
-    }, []);
-
     return (
         <Box className={styles.section}>
             <Container>
