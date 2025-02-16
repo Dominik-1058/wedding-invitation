4742760a8ab9d5fe290b4894395dddf75b4121d7
unknown <reitererdominik@gmail.com>
2025-02-16 15:04:44 +0100

    gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:8000', //TODO: Change this to the backend server
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
+  base: '/wedding-invitation/',
 })
